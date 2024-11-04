b7819339ad53c4ec3f598555aa49fe923c8674f0
Islamov-ak <comdevsw@gmail.com>
2024-11-04 10:34:13 +0100

    Initial Commit

---
+++ WorkshopTest/WorkshopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopTestApp.swift
+//  WorkshopTest
+//
+//  Created by Azizjon Islamov on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
